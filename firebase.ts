7171b6f789169acca6581d9bc1d1f9b82c6bdc13
Hudsonjipa <matosinho2001@gmail.com>
2025-09-29 12:31:40 -0400

    Adiciona configuração real do Firebase

--- firebase.ts
+++ firebase.ts
@@ -1,9 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// TODO: Adicione a configuração do seu projeto Firebase aqui
-// Você pode obter isso no console do Firebase: Configurações do Projeto > Geral > Seus aplicativos > SDK de configuração
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDhuR9Hyao-1pQ9weISzDYsFYRS982aMp4",
   authDomain: "jogo-3-pessoa.firebaseapp.com",
@@ -13,6 +15,7 @@ const firebaseConfig = {
   appId: "1:61972191701:web:ea272cdfbe413fef75855c",
   measurementId: "G-9XR4BNYNST"
 };
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
